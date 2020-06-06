95ff80af133f47db4c437888552f6256454442cc
YigalB <yigalb@hotmail.com>
2020-06-06 20:26:11 +0300

    try1

--- mqtt1/Form1.Designer.cs
+++ mqtt1/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.startMqtt_button = new System.Windows.Forms.Button();
+            this.MQtt_button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startMqtt_button
+            // 
+            this.startMqtt_button.Location = new System.Drawing.Point(62, 36);
+            this.startMqtt_button.Name = "startMqtt_button";
+            this.startMqtt_button.Size = new System.Drawing.Size(135, 42);
+            this.startMqtt_button.TabIndex = 0;
+            this.startMqtt_button.Text = "startMqtt";
+            this.startMqtt_button.UseVisualStyleBackColor = true;
+            this.startMqtt_button.Click += new System.EventHandler(this.startMqtt_button_Click);
+            // 
+            // MQtt_button2
+            // 
+            this.MQtt_button2.Location = new System.Drawing.Point(62, 134);
+            this.MQtt_button2.Name = "MQtt_button2";
+            this.MQtt_button2.Size = new System.Drawing.Size(135, 35);
+            this.MQtt_button2.TabIndex = 1;
+            this.MQtt_button2.Text = "start2";
+            this.MQtt_button2.UseVisualStyleBackColor = true;
+            this.MQtt_button2.Click += new System.EventHandler(this.MQtt_button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MQtt_button2);
+            this.Controls.Add(this.startMqtt_button);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startMqtt_button;
+        private System.Windows.Forms.Button MQtt_button2;
     }
 }
 
--- mqtt1/Form1.cs
+++ mqtt1/Form1.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+//using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace mqtt1
 {
@@ -15,6 +18,41 @@ namespace mqtt1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void startMqtt_button_Click(object sender, EventArgs e)
+        {
+            MqttClient client = new MqttClient("192.168.1.85");
+            //byte code = client.Connect(Guid.NewGuid().ToString());
+
+            byte code = client.Connect(Guid.NewGuid().ToString(), null, null,
+                false, // will retain flag
+                MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, // will QoS
+                true, // will flag
+                "/test", // will topic
+                "will_message", // will message
+                true,60);
+
+            Console.WriteLine("Code is: " + code.ToString());
+            Console.WriteLine("Guid is: " + Guid.NewGuid().ToString());
+            ushort msgId = client.Publish("/test", // topic
+                              Encoding.UTF8.GetBytes("MyMessageBody"), // message body
+                              MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, // QoS level
+                              false); // retained
+            Console.WriteLine("msgId is: " + msgId.ToString());
+
+        }
+
+        private void MQtt_button2_Click(object sender, EventArgs e)
+        {
+            string MQTT_BROKER_ADDRESS = "192.168.1.85";
+           // MqttClient client = new MqttClient(IPAddress.Parse(MQTT_BROKER_ADDRESS));
+
+            //string clientId = Guid.NewGuid().ToString();
+            //client.Connect(clientId);
+
+            //string strValue = Convert.ToString(value);
+
         }
     }
 }
